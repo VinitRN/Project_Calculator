16f810fbb6e8756150fcd2cbe9a2962b0bf64c87
VinitRN <01fe22bca044@kletech.ac.in>
2024-02-19 16:56:29 +0530

    Extended calculator

---
+++ Extended_cal.java
@@ -0,0 +1,11 @@
+public class Extended_cal extends simple_cal {
+    void square(int num) {
+        System.out.println("Square of number is : " + (num * num));
+    }
+
+    void cube(int num) {
+        System.out.println("Cube of number is : " + (num * num * num));
+    }
+
+}
+
